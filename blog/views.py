01d5fc56c528a84e1cf22ff5f4ff8257402c3965
rladmsrn999@naver.com <rladmsrn999@naver.com>
2021-04-26 20:12:37 +0900

    resize

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import os
+from PIL import Image
 
 
 def home(request):
@@ -10,7 +11,19 @@ def new(request):
     yres = request.GET.get('yres')
     xres=int(xres)
     yres=int(yres)
-    imglist=os.listdir('/home/rladmsrn999/new_rep/blog/static')
+    imglist=os.listdir('/home/rladmsrn999/new_rep/static')  #호스팅용
+    # imglist = os.listdir('./static/original')  #로컬용
+    # imglist = imglist.sort()
+
+    for img in imglist:
+        image=Image.open('/home/rladmsrn999/new_rep/static/original/{}'.format(img))
+        img_resize=image.resize((xres,yres))
+        img_resize.save('/home/rladmsrn999/new_rep/static/make/{}'.format(img))
+
+    imglist = os.listdir('/home/rladmsrn999/new_rep/static/make')
+
+
+
 
     return render(request, 'blog/new.html', {'xres':xres, 'yres':yres, 'imglist':imglist})
 

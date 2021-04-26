33197201ef581d6586ee2ea684eae90e28258e13
rladmsrn999@naver.com <rladmsrn999@naver.com>
2021-04-26 22:56:56 +0900

    'sort'

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,7 @@ def new(request):
     yres=int(yres)
     imglist=os.listdir('/home/rladmsrn999/new_rep/blog/static/original')  #호스팅용
     # imglist = os.listdir('./static/original')  #로컬용
-    # imglist = imglist.sort()
+    imglist.sort()
 
     for img in imglist:
         image=Image.open('/home/rladmsrn999/new_rep/blog/static/original/{}'.format(img))

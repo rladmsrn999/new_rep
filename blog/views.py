e4fe9f4e2333e02df2f2a43cf6d0e843c0b78eaa
rladmsrn999@naver.com <rladmsrn999@naver.com>
2021-05-03 22:46:12 +0900

    'resolution'

--- blog/views.py
+++ blog/views.py
@@ -20,6 +20,111 @@ def new(request):
         img_resize=image.resize((xres,yres))
         img_resize.save('/home/rladmsrn999/new_rep/blog/static/make/{}'.format(img))
 
+    image=Image.new("RGB",(xres,yres),(0,0,0))
+
+    im=image.load()
+    for x in range(xres) :
+        for y in range(yres) :
+            if x>int(xres/2) :
+                im[x,y]=(255,255,255)
+    image.save('/home/rladmsrn999/new_rep/blog/static/make/BW1.png')
+
+
+    im = image.load()
+    for x in range(xres) :
+        for y in range(yres) :
+            if y>int(yres/2) :
+                im[x,y]=(255,255,255)
+    image.save('/home/rladmsrn999/new_rep/blog/static/make/BW2.png')
+
+    im = image.load()
+    for x in range(xres) :
+        for y in range(yres) :
+            if x%2==0 and y<int(yres/2):
+                im[x,y]=(255,255,255)
+            elif x%2==1 and y>=int(yres/2):
+                im[x,y]=(255,255,255)
+    image.save('/home/rladmsrn999/new_rep/blog/static/make/Half-Vstripe.png')
+
+    im = image.load()
+    for x in range(xres) :
+        for y in range(yres) :
+            if y%2==0 and x<int(xres/2):
+                im[x,y]=(255,255,255)
+            elif y%2==1 and x>=int(xres/2):
+                im[x,y]=(255,255,255)
+    image.save('/home/rladmsrn999/new_rep/blog/static/make/Half-hstripe.png')
+
+    im = image.load()
+    for x in range(xres):
+        for y in range(yres):
+            if y % 2 == 0:
+                im[x, y] = (255, 255, 255)
+    image.save('/home/rladmsrn999/new_rep/blog/static/make/Hstripe.png')
+
+    im = image.load()
+    for x in range(xres):
+        for y in range(yres):
+            if x% 2 == 0:
+                im[x, y] = (255, 255, 255)
+    image.save('/home/rladmsrn999/new_rep/blog/static/make/Vstripe.png')
+
+    im = image.load()
+    for x in range(xres):
+        for y in range(yres):
+            if (x > int(xres*7/18) and x < int(xres*13/18)) and (y<int(yres/3) or y>int(yres*2/3)):
+                im[x, y] = (255, 255, 255)
+    image.save('/home/rladmsrn999/new_rep/blog/static/make/Xtalk1.png')
+
+    im = image.load()
+    for x in range(xres):
+        for y in range(yres):
+            if (x > int(xres * 11 / 18) and x < int(xres * 17 / 18)) and (y < int(yres / 3) or y > int(yres * 2 / 3)):
+                im[x, y] = (255, 255, 255)
+    image.save('/home/rladmsrn999/new_rep/blog/static/make/Xtalk2.png')
+
+    im = image.load()
+    for x in range(xres):
+        for y in range(yres):
+            if (x > int(xres * 5 / 18) and x < int(xres * 11 / 18)) and (y < int(yres / 3) or y > int(yres * 2 / 3)):
+                im[x, y] = (255, 255, 255)
+    image.save('/home/rladmsrn999/new_rep/blog/static/make/Xtalk3.png')
+
+    im = image.load()
+    for x in range(xres):
+        for y in range(yres):
+            if (x > int(xres * 1 / 18) and x < int(xres * 7 / 18)) and (y < int(yres / 3) or y > int(yres * 2 / 3)):
+                im[x, y] = (255, 255, 255)
+    image.save('/home/rladmsrn999/new_rep/blog/static/make/Xtalk4.png')
+
+    im = image.load()
+    for x in range(xres):
+        for y in range(yres):
+            if (y > int(yres * 7 / 18) and y < int(yres * 13 / 18)) and (x< int(xres / 3) or y > int(xres * 2 / 3)):
+                im[x, y] = (255, 255, 255)
+    image.save('/home/rladmsrn999/new_rep/blog/static/make/Xtalk5.png')
+
+    im = image.load()
+    for x in range(xres):
+        for y in range(yres):
+            if (y > int(yres * 11 / 18) and y < int(yres * 17 / 18)) and (x < int(xres / 3) or y > int(xres * 2 / 3)):
+                im[x, y] = (255, 255, 255)
+    image.save('/home/rladmsrn999/new_rep/blog/static/make/Xtalk6.png')
+
+    im = image.load()
+    for x in range(xres):
+        for y in range(yres):
+            if (y > int(yres * 5 / 18) and y < int(yres * 11 / 18)) and (x< int(xres / 3) or x > int(xres * 2 / 3)):
+                im[x, y] = (255, 255, 255)
+    image.save('/home/rladmsrn999/new_rep/blog/static/make/Xtalk7.png')
+
+    im = image.load()
+    for x in range(xres):
+        for y in range(yres):
+            if (x > int(xres * 1 / 18) and x < int(xres * 7 / 18)) and (y < int(yres / 3) or y > int(yres * 2 / 3)):
+                im[x, y] = (255, 255, 255)
+    image.save('/home/rladmsrn999/new_rep/blog/static/make/Xtalk8.png')
+
     imglist = os.listdir('/home/rladmsrn999/new_rep/blog/static/make')
     imglist.sort()
 

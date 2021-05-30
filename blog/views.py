fe24eba5d8977f12706b33b87baea3356b728c70
rladmsrn999@naver.com <rladmsrn999@naver.com>
2021-05-30 17:01:49 +0900

    zip

--- blog/views.py
+++ blog/views.py
@@ -200,15 +200,15 @@ def new(request):
     imglist = os.listdir('/home/rladmsrn999/new_rep/blog/static/make')
     imglist.sort()
 
-    imgzip = zipfile.ZipFile('\\home\\rladmsrn999\\new_rep\\blog\\static\\make\\imgzip.zip', 'w')
+    imgzip = zipfile.ZipFile('home\\rladmsrn999\\new_rep\\blog\\static\\make\\imgzip.zip', 'w')
 
-    for folder, subfolders, files in os.walk('\\home\\rladmsrn999\\new_rep\\blog\\static\\original'):
+    for folder, subfolders, files in os.walk('home\\rladmsrn999\\new_rep\\blog\\static\\original'):
 
         for file in files:
             if file.endswith('.png'):
                 imgzip.write(os.path.join(folder, file),
                                   os.path.relpath(os.path.join(folder, file),
-                                                  '\\home\\rladmsrn999\\new_rep\\blog\\static\\original'),
+                                                  'home\\rladmsrn999\\new_rep\\blog\\static\\original'),
                                   compress_type=zipfile.ZIP_DEFLATED)
 
     imgzip.close()
